b977938592567ff189202606f43e23da2c104bdd
Ed Whyatt <lizardxyz@googlemail.com>
2022-05-26 15:13:31 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function chackAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore(){
+ 
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+    
+}

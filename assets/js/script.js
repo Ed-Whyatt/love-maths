e221ed65309414303bd7cb258cf16ad6a8d3bba2
Ed Whyatt <lizardxyz@googlemail.com>
2022-05-27 16:21:57 +0000

    Add subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,8 @@ function runGame(gameType) {
         displayAdditionQuestion (num1, num2);
     } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract"){
+        displaySubtractQuestion (num1, num2);
     } else { 
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}.aborting!`;
@@ -75,7 +77,9 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    }else {
+    } else if (operator === "-") {
+        return[operand1 - operand2, "subtract"];
+    } else {
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}.Aborting!`;
     }
@@ -110,9 +114,11 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
 
-    
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 
 }
 
